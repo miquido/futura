66b5c735a94f411fe72f94d94b2c47d1b709334c
kaqu <kaqukal@gmail.com>
2018-10-01 14:01:43 +0200

    cleanup

--- Futura/Futura/Worker.swift
+++ Futura/Futura/Worker.swift
@@ -12,11 +12,6 @@
  See the License for the specific language governing permissions and
  limitations under the License. */
 
-/// worker using main thread/queue of application
-public var mainWorker: Worker = DispatchWorker.main
-/// default async worker
-public var asyncWorker: Worker = DispatchWorker.default
-
 /// Protocol used to describe worker behaviour. This abstraction allows to use DispatchQueue and other threading solutions for Futures.
 public protocol Worker {
     /// Schedule should notify worker to execute given task asynchronously by its own rules without blocking current thread
