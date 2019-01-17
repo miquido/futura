74d7eb88c32ef022ec6d841a03cfd76ad2d7de9e
kaqu <kaqukal@gmail.com>
2019-01-17 14:50:17 +0100

    fix docs

--- Sources/Futura/Signal/SignalMerger.swift
+++ Sources/Futura/Signal/SignalMerger.swift
@@ -1,11 +1,11 @@
 /* Copyright 2018 Miquido
- 
+
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
- 
+
  http://www.apache.org/licenses/LICENSE-2.0
- 
+
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,17 +15,19 @@
 /// Merges Signals into new Signal that will emit whenever one of merged Signals emit.
 /// When any of merged Signals becomes finished merged Signal also becomes finished.
 ///
-/// - Paramerer signals: Signals to merge
+/// - Warning: Result signal will not inherit collector from any of provided Signals.
+///
+/// - Parameter signals: Signals to merge
 /// - Returns: New Signal instance merging provided Signals
 public func merge<Value>(_ signals: Signal<Value>...) -> Signal<Value> {
     let mergedSignal: Signal<Value> = .init(collector: nil)
     signals.forEach { signal in
         mergedSignal.collect(signal.subscribe({ event in
             switch event {
-            case let .token(token):
-                mergedSignal.broadcast(token)
-            case let .finish(reason):
-                mergedSignal.finish(reason)
+                case let .token(token):
+                    mergedSignal.broadcast(token)
+                case let .finish(reason):
+                    mergedSignal.finish(reason)
             }
         }))
     }
